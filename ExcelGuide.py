a02aea77c545cc491499b0b45e71b8eeecd40c83
lwhyumao <muyuan.weihao.liang@gmail.com>
2019-04-27 23:46:51 +0800

    提交数据

--- ExcelGuide.py
+++ ExcelGuide.py
@@ -1,1 +1,18 @@
 import xlrd
+
+
+class excelGuide:
+    def __init__(self, excel_Name=input()):
+        print("请输入需要导出表格的名字")
+        try:
+            self.excel_data = xlrd.open_workbook(excel_Name)
+            return self.excel_data
+        except:
+            print("")
+
+    def sheet_Data(self):
+        self.sheet_Data = self.excel_data.sheet_names()
+
+
+d = excelGuide()
+d.sheet_Data()

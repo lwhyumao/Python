f61253d720716dba0cf07b7216863894dbf1be1c
liangweihao <muyuan.weihao.liang@gmail.com>
2019-04-28 11:32:51 +0800

    提交代码

--- ExcelGuide.py
+++ ExcelGuide.py
@@ -6,13 +6,17 @@ class excelGuide:
         print("请输入需要导出表格的名字")
         try:
             self.excel_data = xlrd.open_workbook(excel_Name)
-            return self.excel_data
         except:
-            print("")
+            print("请输入正确的表格名字")
 
-    def sheet_Data(self):
-        self.sheet_Data = self.excel_data.sheet_names()
+    def sheet_Data(self,sheet_name = input()):
+        print("请输入需要导出的表格工作薄名字")
+        try:
+            self.sheet_Data = self.excel_data.sheet_names(sheet_name)
+        except:
+            print("工作薄名字不正确")
 
 
 d = excelGuide()
 d.sheet_Data()
+
